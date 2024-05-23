6c3b298b85504dd105126851003803eecbf8c893
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-05-24 02:05:53 +0600

    Menu Screen has been modified with enddrawer.

--- lib/presentation/screens/menu_screen.dart
+++ lib/presentation/screens/menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 import 'package:qp_finance/presentation/utility/imageasset.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -10,39 +11,53 @@ class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 94,
-          width: 370,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '',
-                      style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
+      body: Column(
+        children: [
+          _buildContainer(
+              childLeft: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  height: 54,
+                  width: 50,
+                  child: Image.network(
+                      fit: BoxFit.cover,
+                      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D'),
                 ),
-                SvgPicture.asset(ImageAssets.rewardlogoSVG),
-              ],
-            ),
+              ),
+              childRight: const Text("Rokeya Mim")),
+          Gap(10),
+          _buildContainer(
+            childLeft: SvgPicture.asset(ImageAssets.rewardlogoSVG),
+            childRight: Text('Group'),
           ),
+          Gap(5),
+          _buildContainer(
+            childLeft: SvgPicture.asset(ImageAssets.rewardlogoSVG),
+            childRight: Text('Group'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildContainer(
+      {required Widget childLeft, required Widget childRight}) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: Colors.white,
+          width: 1,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [childLeft, const Gap(10), childRight],
         ),
       ),
     );
